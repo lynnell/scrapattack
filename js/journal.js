19585aaca8879406929cb405a14501cb0ba738c8
rails new blog <jddriscoll@hotmail.com>
2014-05-03 23:13:03 -0500

    new interface for journals

---
+++ js/journal.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+	//##### send add record Ajax request to response.php #########
+	$("#FormSubmit").click(function (e) {
+			e.preventDefault();
+			if($("#contentText").val()==='')
+			{
+				alert("Please enter some text!");
+				return false;
+			}
+		 	var myData = 'content_txt='+ $("#contentText").val(); //build a post data structure
+			jQuery.ajax({
+			type: "POST", // HTTP method POST or GET
+			url: "../include/response.php", //Where to make Ajax calls
+			dataType:"text", // Data type, HTML, json etc.
+			data:myData, //Form variables
+			success:function(response){
+				$("#responds").append(response);
+				$("#contentText").val(''); //empty text field on successful
+			},
+			error:function (xhr, ajaxOptions, thrownError){
+				alert(thrownError);
+			}
+			});
+	});
+
+	//##### Send delete Ajax request to response.php #########
+	$("body").on("click", "#responds .del_button", function(e) {
+	alert("test point journal.js");
+		 e.returnValue = false;
+		 var clickedID = this.id.split('-'); //Split string (Split works as PHP explode)
+		 var DbNumberID = clickedID[1]; //and get number from array
+		 var myData = 'recordToDelete='+ DbNumberID; //build a post data structure
+		 
+			jQuery.ajax({
+			type: "POST", // HTTP method POST or GET
+			url: "../include/response.php", //Where to make Ajax calls
+			dataType:"text", // Data type, HTML, json etc.
+			data:myData, //Form variables
+			success:function(response){
+				//on success, hide  element user wants to delete.
+				$('#item_'+DbNumberID).fadeOut("slow");
+			},
+			error:function (xhr, ajaxOptions, thrownError){
+				//On error, we alert user
+				alert(thrownError);
+			}
+			});
+	});
+
+});
